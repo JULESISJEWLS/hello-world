08e3665c5e059fd74fc41db3b21d23451f4b69e5
JULESISJEWLS <118814987+JULESISJEWLS@users.noreply.github.com>
2023-10-26 13:30:11 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,4 +17,14 @@ self.addEventListener('install', (event) => {
       })
     );
   });
-  
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
